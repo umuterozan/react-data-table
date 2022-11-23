7196b1b37830140648805e09f3518a8a17a87c03
Umut Erozan <umut_erozan@hotmail.com>
2022-11-23 20:25:57 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import Table from "./data-table/table";
+import {data} from "./data-table/data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    const [users, setUsers] = useState(() => data)
+
+    return (
+        <div className="p-4">
+            <Table
+                searchable={true}
+                head={[
+                    { name: 'Ad-Soyad', sortable: true },
+                    { name: 'E-posta' },
+                    { name: 'Yaş', sortable: true },
+                    { name: 'İşlemler', width: 200, style: { textAlign: "right" } }
+                ]}
+                body={users && users.map((user, key) => ([
+                    `${user.name} ${user.surname}`,
+                    user.email,
+                    user.age,
+                    [
+                        <button className="h-8 px-4 flex items-center justify-center rounded bg-blue-600 text-white">Düzenle</button>,
+                        <button onClick={() => {
+                            const tmpUsers = [...users]
+                            tmpUsers.splice(key, 1)
+                            setUsers(tmpUsers)
+                        }} className="h-8 px-4 flex items-center justify-center rounded bg-red-600 text-white">Sil</button>
+                    ]
+                ]))}
+            />
+        </div>
+    )
 }
-
-export default App;
---
+++ src/data-table/data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        name: 'John',
+        surname: 'Doe',
+        email: 'johndoe@gmail.com',
+        age: 24
+    },
+    {
+        name: 'Leanne',
+        surname: 'Graham',
+        email: 'lgraham@gmail.com',
+        age: 29
+    },
+    {
+        name: 'Ervin',
+        surname: 'Howell',
+        email: 'ervinhowell@gmail.com',
+        age: 35
+    },
+    {
+        name: 'Clementine',
+        surname: 'Bauch',
+        email: 'cbauch@gmail.com',
+        age: 42
+    }
+]
